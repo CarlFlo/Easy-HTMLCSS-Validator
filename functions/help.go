00bc6de9b404e8e993c71b804e0eff0fab3122f7
CarlFlo <39366974+CarlFlo@users.noreply.github.com>
2018-06-15 15:02:15 +0200

    Add files via upload

--- functions/help.go
+++ functions/help.go
@@ -1,26 +1,34 @@
 package functions
 
 import (
+	"bytes"
 	"io/ioutil"
 	"log"
 	"os"
 )
-
-var helpTxt = `CONFIG
-\ncores: 
-\nfolderName: 
-\nreqHTMLFolder: 
-\nmakeHelpTxt: 
-`
 
 // MakeHelpFile creates a helper file
 func MakeHelpFile() {
 
 	if _, err := os.Stat("./help.txt"); os.IsNotExist(err) {
 
-		err = ioutil.WriteFile("help.txt", []byte(helpTxt), 0644)
+		err = ioutil.WriteFile("help.txt", createString(), 0644)
 		if err != nil {
 			log.Println(err.Error())
 		}
 	}
+}
+
+// Ugly solution
+func createString() []byte {
+
+	var buffer bytes.Buffer
+
+	buffer.WriteString("CONFIG")
+	buffer.WriteString("\ncores: How many cores the program can use (def=1)")
+	buffer.WriteString("\nfolderName: The folder name the program will use for bulk validation")
+	buffer.WriteString("\nmakeHelpTxt: If this txt file is to be created (def=true)")
+	buffer.WriteString("\n\n You can both drag and drop a zip file or folder with html files inside them onto the exe file to validate just those files")
+
+	return buffer.Bytes()
 }
