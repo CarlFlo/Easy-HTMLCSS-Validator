b9eef5aa1e5a0ddcefd45b1e7381a1f7a7cd53ce
CarlFlo <39366974+CarlFlo@users.noreply.github.com>
2018-06-26 16:11:50 +0200

    Add files via upload

--- src/functions/save.go
+++ src/functions/save.go
@@ -16,7 +16,8 @@ func SaveResult(list *Work) {
 
 	// Saving file
 	jsonDataJSON, _ := json.MarshalIndent(list, "", "   ")
-	ioutil.WriteFile("output.js", []byte(fmt.Sprintf("var OUTPUT_RESULT = %s", string(jsonDataJSON))), 0644)
+
+	ioutil.WriteFile(fmt.Sprintf("%s/output.js", getExeDir()), []byte(fmt.Sprintf("var OUTPUT_RESULT = %s", string(jsonDataJSON))), 0644)
 
 	openSite()
 }
