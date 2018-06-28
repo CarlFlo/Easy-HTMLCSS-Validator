3f64298547c86e3c63137dbada73dba3f466a3af
CarlFlo <39366974+CarlFlo@users.noreply.github.com>
2018-06-28 11:42:01 +0200

    Add files via upload

---
+++ src/functions/util.go
@@ -0,0 +1,83 @@
+package functions
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"time"
+)
+
+// DeleteOldOutput deletes the old output file
+func DeleteOldOutput() {
+
+	path := fmt.Sprintf("%s/output.js", getExeDir())
+
+	if _, err := os.Stat(path); err == nil {
+		os.Remove(path)
+	}
+
+}
+
+// Get full dir of exe
+func getExeDir() string {
+
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return dir
+}
+
+// Clear will clear the screen
+func Clear() {
+
+	// Check os and use correct settings
+	switch currentOS := runtime.GOOS; currentOS {
+	case "linux":
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	case "windows":
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	default:
+		log.Println(fmt.Sprintf("Currently running on %s. No clear setting for this type", currentOS))
+	}
+}
+
+// ExitGoroutine will stop the goroutine that calls this function
+func ExitGoroutine() {
+	runtime.Goexit()
+}
+
+// SleepMs will sleep for n milliseconds
+func SleepMs(n time.Duration) {
+	time.Sleep(time.Millisecond * n)
+}
+
+// SetCmdSize sets the cmd size //
+func SetCmdSize(cols, lines string) {
+
+	cmd := exec.Command("mode", "con:", fmt.Sprintf("cols=%s", cols), fmt.Sprintf("lines=%s", lines))
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+// RandomString returns a random string of n length
+func RandomString(n int) string {
+
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
